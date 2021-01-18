f1d3837f111013e33af1ce2aff68610574bc37ff
SymbolZH <61349710+SymbolZH@users.noreply.github.com>
2021-01-18 16:15:22 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -22,44 +22,11 @@ public class Main {
         Scanner scanner;
         scanner = new Scanner(input);
 
-        var iter= fun_scan(scanner);
-        //var iter = new StringIter(scanner);
+        //var iter= fun_scan(scanner);
+        var iter = new StringIter(scanner);
         var tokenizer = fun_tokenize(iter);
         var tokens = new ArrayList<Token>();
-        switch (args[0]){
-            case "--tokenaize":{
-                try {
-                    while (true) {
-                        var token = tokenizer.nextToken();
-                        if (token.getTokenType().equals(TokenType.EOF)) {
-                            break;
-                        }
-                        tokens.add(token);
-                        System.out.println(token.toString());
-                    }
-                } catch (Exception e) {
-                    // 遇到错误不输出，直接退出
-                    System.err.println(e);
-                    System.exit(0);
-                    return;
-                }
-                break;
-            }
-            case "--analyse":{
-                var analyzer = new Analyser(tokenizer);
-                List<Instruction> instructions;
-                analyzer.analyse();
-                break;
-            }
-            case "--generate":{
-                var analyzer = new Analyser(tokenizer);
-                analyzer.analyse();
-                var generator=new Generator(dos,analyzer);
-                generator.generateo0();
-                break;
-            }
-        }
-        /*
+
         if(args[0].equals("--tokenaize")){
             try {
                 while (true) {
@@ -87,7 +54,7 @@ public class Main {
             analyzer.analyse();
             var generator=new Generator(dos,analyzer);
             generator.generateo0();
-        }*/
+        }
     }
     private static Tokenizer fun_tokenize(StringIter iter) {
         var tokenizer = new Tokenizer(iter);
